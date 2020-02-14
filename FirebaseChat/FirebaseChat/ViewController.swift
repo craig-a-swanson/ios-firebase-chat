5019b57e9e13fcef916b2ca048d75385ef0f884d
Craig Swanson <cswanson02@me.com>
2020-02-13 23:54:42 -0600

    Created project file

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Craig Swanson on 2/13/20.
+//  Copyright © 2020 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
