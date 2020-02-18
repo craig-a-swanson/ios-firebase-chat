0d3198dcf75d00806df03d866bdfd4c1d88fa334
Craig Swanson <cswanson02@me.com>
2020-02-17 20:10:00 -0600

    refactoring for encoding and decoding.

--- FirebaseChat/FirebaseChat/Model Controller/ChatMessageModelController.swift
+++ FirebaseChat/FirebaseChat/Model Controller/ChatMessageModelController.swift
@@ -41,102 +41,70 @@ class ChatMessageController {
         
         // set the firebase reference and an observer
         databaseReference = Database.database().reference()
-        databaseReference.child("chatRoom").observe(.value) { (snapshot) in
-            // code to execute when a child is added under "chatRoom"
-            // take the value from snapshot and add it to the array for the tableview datasource
-            let possibleRoom = snapshot.value as? [String: Any]
-            
-            guard let actualRoom = possibleRoom,
-                let roomName = actualRoom["roomName"] as? String,
-                let roomID = actualRoom["roomID"] as? String else { return }
-            
-            var possibleMessages: [ChatRoom.Message] = []
-            
-            self.databaseReference.child("chatRoom/messages").observeSingleEvent(of: .value) { (messageSnapshot) in
-                let possibleMessage = messageSnapshot.value as? [String: Any]
-                
-                guard let actualMessage = possibleMessage,
-                let messageID = actualMessage["messageID"] as? String,
-                let timestamp = actualMessage["timestamp"] as? NSNumber,
-                let displayName = actualMessage["displayName"] as? String,
-                let senderId = actualMessage["senderId"] as? String,
-                    let text = actualMessage["text"] as? String else { return }
-                
-                let currentSender = Sender(senderId: senderId, displayName: displayName)
-                
-                let formatter: DateFormatter = {
-                    let result = DateFormatter()
-                    result.dateStyle = .medium
-                    result.timeStyle = .medium
-                    return result
-                }()
-                
-                let messageTimestamp = Date(timeIntervalSince1970: TimeInterval(timestamp))
-//                guard let messageTimestamp = formatter.date(from: timestamp) else { return }
-                
-                let newMessage = ChatRoom.Message(messageText: text, sender: currentSender, timestamp: messageTimestamp, messageId: messageID)
-                possibleMessages.append(newMessage)
+        
+        databaseReference.child("chatRoom").observeSingleEvent(of: .value) { (snapshot) in
+            guard let chatroomsByID = snapshot.value as? [String: ChatRoom] else {
+                return
             }
-            
-            let newRoom = ChatRoom(roomName: roomName, roomID: roomID, messages: possibleMessages)
-            
-            self.chatRooms.append(newRoom)
-            print(self.chatRooms.count)
-            
+            let chatRooms = Array<ChatRoom>(chatroomsByID.values)
+            self.chatRooms = chatRooms
+            completion()
         }
         
-//        let requestURL = ChatMessageController.baseURL.appendingPathExtension("json")
-//
-//        URLSession.shared.dataTask(with: requestURL) { (possibleData, _, possibleError) in
-//            if let error = possibleError {
-//                NSLog("Error fetching chat rooms: \(error)")
-//                completion()
-//                return
+//        
+//        databaseReference.child("chatRoom").observe(.value) { (snapshot) in
+//            // code to execute when a child is added under "chatRoom"
+//            // take the value from snapshot and add it to the array for the tableview datasource
+//            let possibleRoom = snapshot.value as? [String: Any]
+//            
+//            guard let actualRoom = possibleRoom,
+//                let roomName = actualRoom["roomName"] as? String,
+//                let roomID = actualRoom["roomID"] as? String else { return }
+//            
+//            var possibleMessages: [ChatRoom.Message] = []
+//            
+//            self.databaseReference.child("chatRoom/messages").observeSingleEvent(of: .value) { (messageSnapshot) in
+//                let possibleMessage = messageSnapshot.value as? [String: Any]
+//                
+//                guard let actualMessage = possibleMessage,
+//                let messageID = actualMessage["messageID"] as? String,
+//                let timestamp = actualMessage["timestamp"] as? NSNumber,
+//                let displayName = actualMessage["displayName"] as? String,
+//                let senderId = actualMessage["senderId"] as? String,
+//                    let text = actualMessage["text"] as? String else { return }
+//                
+//                let currentSender = Sender(senderId: senderId, displayName: displayName)
+//                
+//                let formatter: DateFormatter = {
+//                    let result = DateFormatter()
+//                    result.dateStyle = .medium
+//                    result.timeStyle = .medium
+//                    return result
+//                }()
+//                
+//                let messageTimestamp = Date(timeIntervalSince1970: TimeInterval(timestamp))
+////                guard let messageTimestamp = formatter.date(from: timestamp) else { return }
+//                
+//                let newMessage = ChatRoom.Message(messageText: text, sender: currentSender, timestamp: messageTimestamp, messageId: messageID)
+//                possibleMessages.append(newMessage)
 //            }
-//
-//            guard let data = possibleData else {
-//                NSLog("No data returned from fetch Chat Rooms data task")
-//                completion()
-//                return
-//            }
-//
-//            do {
-//                self.chatRooms = try JSONDecoder().decode([String: ChatRoom].self, from: data).map({ $0.value })
-//            } catch {
-//                self.chatRooms = []
-//                NSLog("Error decoding chat rooms from JSON data: \(error)")
-//            }
-//            completion()
-//        }.resume()
+//            
+//            let newRoom = ChatRoom(roomName: roomName, roomID: roomID, messages: possibleMessages)
+//            
+//            self.chatRooms.append(newRoom)
+//            print(self.chatRooms.count)
+//            
+//        }
     }
     
-    func createChatRoom(with roomName: String, completion: @escaping () -> Void) {
+    func createChatRoom(with chatroom: ChatRoom, completion: @escaping (Error?) -> Void) {
         
         databaseReference = Database.database().reference()
-        databaseReference.child("chatRoom").setValue(["title":roomName, "identifier":UUID().uuidString])
+        let newRoomReference = databaseReference.child("chatRoom").child(chatroom.roomID)
         
-        let room = ChatRoom(roomName: roomName)
-        
-//        let requestURL = ChatMessageController.baseURL.appendingPathComponent(room.roomID).appendingPathExtension("json")
-//        var request = URLRequest(url: requestURL)
-//        request.httpMethod = "PUT"
-//
-//        do {
-//            request.httpBody = try JSONEncoder().encode(room)
-//        } catch {
-//            NSLog("Error encoding chat room name to JSON: \(error)")
-//        }
-//
-//        URLSession.shared.dataTask(with: request) { (possibleData, _, possibleError) in
-//            if let error = possibleError {
-//                NSLog("Error with chat room creation data task: \(error)")
-//                completion()
-//                return
-//            }
-//            self.chatRooms.append(room)
-//            completion()
-//
-//        }.resume()
+        newRoomReference.setValue(chatroom) { error, _ in
+            completion(error)
+        }
     }
     
     func createMessage(in chatRoom: ChatRoom, withText text: String, sender: Sender, completion: @escaping () -> Void) {
@@ -147,27 +115,5 @@ class ChatMessageController {
         
         chatRooms[index].messages.append(message)
         
-//        let requestURL = ChatMessageController.self.baseURL.appendingPathComponent(chatRoom.roomID).appendingPathComponent("messages").appendingPathExtension("json")
-//        
-//        var request = URLRequest(url: requestURL)
-//        request.httpMethod = "POST"
-//        
-//        do {
-//            request.httpBody = try JSONEncoder().encode(message)
-//        } catch {
-//            NSLog("Error encoding message to JSON: \(error)")
-//        }
-//        
-//        URLSession.shared.dataTask(with: request) { (data, _, error) in
-//            
-//            if let error = error {
-//                NSLog("Error with message thread creation data task: \(error)")
-//                completion()
-//                return
-//            }
-//            
-//            completion()
-//            
-//        }.resume()
     }
 }
